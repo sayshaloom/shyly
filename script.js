364a2aefd40b17deba38803226f0e532424631dd
sayshaloom <dariamakarova2905@gmail.com>
2025-12-05 13:24:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-    for (let i = 0; i < 12; i++) {
-    document.body.innerHTML += "HHHHHH<br>";
+for (let i = 0; i < 12; i++) {
+    document.body.innerHTML += "HHHHHH                  HHHHHH<br>";
+}
+
+for (let i = 0; i < 4; i++) {
+    document.body.innerHTML += "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHH<br>";
 }
