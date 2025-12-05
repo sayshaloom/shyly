f0a46c1cf22788d1fd260fe42f2fe5a1d4db5bf9
sayshaloom <dariamakarova2905@gmail.com>
2025-12-05 13:37:09 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-for (let i = 0; i < 12; i++) {
-    document.body.innerHTML += "HHHHHH                  HHHHHH<br>";
-}
-
-for (let i = 0; i < 4; i++) {
-    document.body.innerHTML += "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHH<br>";
-}
